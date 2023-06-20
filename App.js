1b64fcc6527b329b8436635b98d98f1d3c81e6d6
GuilhermeProença <127969945+GuilhermeProencaCorrea@users.noreply.github.com>
2023-06-19 21:35:01 -0300

    :toda: Tela inicial

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, useState}from 'expo-status-bar';
+import { StyleSheet, Text, View,SafeAreaView, TouchableHighlight,TextInput} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+export default function calcimc(){
+
+  const [peso,setPeso]=useState(0)
+  const [altura,setAltura]=useState(0)
+  const [resultado,setResultado]=useState(0)
+
+  return(
+    <View>
+    <Text>Meu App</Text>
+    <View>
+      <text>Informe seu peso:</text>
+      <TextInput>
+        autoFocus={true}
+        keyboardType={'numeric'}
+        value={peso}
+        onChangeText={text=>setPeso(text)}
+      </TextInput>
+
+
     </View>
-  );
+
+    </View>
+  )
 }
+
 
 const styles = StyleSheet.create({
   container: {
